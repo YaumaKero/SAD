196111eec608e958e51c2e6f6d0324a1a18f87ee
Gonsacu01 <125359370+Gonsacu01@users.noreply.github.com>
2023-02-17 10:22:52 +0100

    Update EditableBufferedReader.java

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -28,11 +28,49 @@ public class EditableBufferedReader extends BufferedRead {
         char character = scanner.next().charAt(0);
     }
     public void readLine(){
-        while(true){        
-            try {
-                str = in.readLine();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        Line line = new Line();
+        char x;
+        while(line.text.charAt(line.cursorPos)=='\n'){
+            x=(char)in.read();
+            switch(x){
+                case '\n':
+                    break;
+                
+                case 27:
+                    in.read();
+                    x=(char)in.read();
+                    if(x==68)
+                        line.cursorPos--;
+                    else if(x==67)
+                        line.cursorPos++;
+                    else if(x==72)
+                        line.cursorPos=0;
+                    else if(x==70)
+                        line.cursorPos=line.text.length();
+                    else if(x==50){                              //puede dar problemas pq hay un caracter mas que en las otras teclas
+                        line.editMode=!line.editMode;
+                        in.read();                               //esto debería arreglarlo
+                    }
+                    break;
+                
+                
+                default:
+                    if(line.editMode==line.SUBSTITUTION){
+                        StringBuilder sb = new StringBuilder(line.text);
+                        sb.setCharAt(line.cursorPos, x);
+                        line.text=sb.toString();
+                    }
+                    else if(line.editMode==line.INSERTION){
+                        StringBuilder stringBuilder= new StringBuilder(line.text);
+                        stringBuilder.insert(line.cursorPos,x);
+                        line.text=stringBuilder.toString();
+                    }
+            
+                    line.cursorPos++;
             }
-        }   
+        
+        }
     }  
 }
 
