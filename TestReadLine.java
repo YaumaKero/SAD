8f79fdc10f56b4743f11c0796363f4e01be09280
Gonsacu01 <125359370+Gonsacu01@users.noreply.github.com>
2023-03-03 09:57:59 +0100

    Update TestReadLine.java

--- TestReadLine.java
+++ TestReadLine.java
@@ -2,12 +2,10 @@ import java.io.*;
 
 class TestReadLine {
   public static void main(String[] args) {
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
     String str = null;
-    int a = 0;
     try {
-      //str = in.readLine();
-      a = in.read();
+      str = in.readLine();
     } catch (IOException e) { e.printStackTrace(); }
     System.out.println("\nline is: " + str);
   }
