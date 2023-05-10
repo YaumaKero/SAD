60783138ab399dd04e95613211f2d513dab3d286
Gonsacu01 <125359370+Gonsacu01@users.noreply.github.com>
2023-05-10 08:35:15 +0200

    server.py

--- prácticaFINAL/server.py
+++ prácticaFINAL/server.py
@@ -1,44 +1,33 @@
 import socket
-import threading
 import pickle
-
-class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.host, self.port))
-        self.clients = []
-        self.decks = {}
-
-    def start(self):
-        self.socket.listen()
-        print(f"Server listening on {self.host}:{self.port}")
+import deck
+    
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('localhost', 8000))
+        s.listen()
         while True:
-            client_socket, client_address = self.socket.accept()
-            print(f"New client connected from {client_address}")
-            self.clients.append(client_socket)
-            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+            conn, addr = s.accept()
+            with conn:
+                print(f'Conexión establecida con {addr}')
+                with conn:
+                    # Recibir los datos del cliente
+                    data = conn.recv(4096)
+                    # Decodificar los datos como una cadena
+                    mensaje = data.decode()
+                    # Verificar si el mensaje es de subida o envío de mazos
+                    if mensaje.startswith('upload'):
+                        # Quitar el prefijo del mensaje
+                        datos = mensaje[len('upload:'):]
+                        # Deserializar el objeto Mazos
+                        mazos = pickle.loads(datos.encode())
+                        # Sobreescribir el objeto Mazos
+                        print(f'Se ha sobrescrito el objeto: {mazos}')
+                    elif mensaje.startswith('download'):
+                        # Serializar el objeto Mazos
+                        datos = pickle.dumps(mazos)
+                        # Crear un mensaje que indique que se está enviando un objeto Mazos
+                        mensaje = f'mazos:{datos.decode()}'
+                        # Enviar el mensaje al cliente
+                        conn.sendall(mensaje.encode())
 
-    def handle_client(self, client_socket):
-        while True:
-            try:
-                data = client_socket.recv(4096)
-                if data:
-                    message = pickle.loads(data)
-                    if message["action"] == "add_deck":
-                        self.decks[message["deck_name"]] = []
-                    elif message["action"] == "add_card":
-                        self.decks[message["deck_name"]].append(message["card"])
-                    elif message["action"] == "get_decks":
-                        response = {"decks": list(self.decks.keys())}
-                        client_socket.send(pickle.dumps(response))
-                    elif message["action"] == "get_cards":
-                        deck_name = message["deck_name"]
-                        response = {"cards": self.decks[deck_name]}
-                        client_socket.send(pickle.dumps(response))
-            except Exception as e:
-                print(f"Error handling client: {e}")
-                self.clients.remove(client_socket)
-                client_socket.close()
-                break
