3409a326ecbbc0885ebae49e53a131edce67a8a4
Gonsacu01 <125359370+Gonsacu01@users.noreply.github.com>
2023-05-13 11:07:53 +0200

    Add files via upload

--- prácticaFINAL/client.py
+++ prácticaFINAL/client.py
@@ -1,34 +1,37 @@
+import io
 import socket
 import pickle
-import deck
+import time
 
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('localhost', 8000))
-        # Solicitar los mazos al servidor
-        with s:
-            # Enviar la solicitud de mazos al servidor
-            s.sendall('download'.encode())
-            # Recibir los datos del servidor
-            data = s.recv(4096)
-            # Decodificar los datos como una cadena
-            mensaje = data.decode()
-            # Verificar si el mensaje es de un objeto Mazos
-            if mensaje.startswith('mazos:'):
-                # Quitar el prefijo del mensaje
-                datos = mensaje[len('mazos:'):]
-                # Deserializar el objeto Mazos
-                mazos = pickle.loads(datos.encode())
-                # Hacer algo con el objeto recibido
-                print(f'Se ha recibido el objeto: {mazos}')
-        # Hacer algo con los mazos recibidos
+#def main():
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 8000))
+mazos = []
+# Enviar la solicitud de mazos al servidor
+s.sendall(pickle.dumps('download'))
+# Recibir los datos del servidor
+data = s.recv(4096)
 
-        #...
+print(data)
+    # Deserializar el objeto Mazos
+mazos = pickle.loads(data)
+    # Hacer algo con el objeto recibido
+print('Se ha recibido el objeto: {}'.format(mazos))
+# Hacer algo con los mazos recibidos
+s.close()
+mazos.append(1)
+time.sleep(2)
+print(mazos)
+# Enviar los mazos al servidor
+# Serializar el objeto Mazos
+datos = pickle.dumps(mazos)
+print(datos)
 
-        # Enviar los mazos al servidor
-        # Serializar el objeto Mazos
-        datos = pickle.dumps(deck)
-        # Crear un mensaje que indique que se está enviando un objeto Mazos
-        mensaje = f'mazos:{datos.decode()}'
-        # Enviar el mensaje al servidor
-        s.sendall(mensaje.encode())
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 8000))
+# Crear un mensaje que indique que se está enviando un objeto Mazos
+#mensaje = 'upload:{}'.format(datos.decode('utf-8'))
+# Enviar el mensaje al servidor
+s.sendall(datos)
+s.close()
+print("conexion cerrada")
--- prácticaFINAL/server.py
+++ prácticaFINAL/server.py
@@ -1,33 +1,42 @@
 import socket
 import pickle
-import deck
     
-def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', 8000))
-        s.listen()
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print(f'Conexión establecida con {addr}')
-                with conn:
-                    # Recibir los datos del cliente
-                    data = conn.recv(4096)
-                    # Decodificar los datos como una cadena
-                    mensaje = data.decode()
-                    # Verificar si el mensaje es de subida o envío de mazos
-                    if mensaje.startswith('upload'):
-                        # Quitar el prefijo del mensaje
-                        datos = mensaje[len('upload:'):]
-                        # Deserializar el objeto Mazos
-                        mazos = pickle.loads(datos.encode())
-                        # Sobreescribir el objeto Mazos
-                        print(f'Se ha sobrescrito el objeto: {mazos}')
-                    elif mensaje.startswith('download'):
-                        # Serializar el objeto Mazos
-                        datos = pickle.dumps(mazos)
-                        # Crear un mensaje que indique que se está enviando un objeto Mazos
-                        mensaje = f'mazos:{datos.decode()}'
-                        # Enviar el mensaje al cliente
-                        conn.sendall(mensaje.encode())
-
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('localhost', 8000))
+    mazos = [1,2,3]
+    s.listen()
+        
+    while True:
+        print("esperando aceptación")
+        conn, addr = s.accept()
+    
+        print('Conexión establecida con {}'.format(addr))
+    
+        # Recibir los datos del cliente
+        data = conn.recv(4096)
+        # Decodificar los datos como una cadena
+        mensaje = pickle.loads(data)
+        #mensaje = data.decode('utf-8')
+        print(mensaje)
+        # Verificar si el mensaje es de subida o envío de mazos
+        if isinstance(mensaje,str):
+            # Serializar el objeto Mazos
+            datos = pickle.dumps(mazos)
+            print(datos)
+            # Crear un mensaje que indique que se está enviando un objeto Mazos
+            mensaje = 'mazos:{}'.format(datos)
+            # Enviar el mensaje al cliente
+            conn.sendall(datos)
+            conn.close()
+        else:
+            print("He recibido: ")
+            print(mensaje)
+            # Quitar el prefijo del mensaje
+            #datos = mensaje[len('upload:'):]
+            # Deserializar el objeto Mazos
+            mazos = mensaje
+            # Sobreescribir el objeto Mazos
+            print('Se ha sobrescrito el objeto: {}'.format(mazos))
+            print(mazos)
+            conn.close()
+                
